b42ffc9e909be9d825ac735cb02583fd8877a47a
Sebastian Beraud <beraudsebastian@gmail.com>
2020-12-14 18:10:32 +0100

    hiding menu when clicking on name

--- js/script.js
+++ js/script.js
@@ -2,10 +2,14 @@ document.addEventListener('DOMContentLoaded', nav)
 
 function nav(){
     let menuToggle = document.querySelectorAll('.menu-toggle');
+    const menuHide = document.querySelector('.menu-hide')
     const nav = document.querySelector('.navbar');
     menuToggle.forEach(function(element) {
       element.addEventListener('click', () => {
         nav.classList.toggle('show')
       })
+    })
+    menuHide.addEventListener('click', () => {
+      nav.classList.remove('show')
     })
 }
